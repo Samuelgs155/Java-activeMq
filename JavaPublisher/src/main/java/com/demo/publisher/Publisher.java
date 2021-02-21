18357e513746c2a22b469b2fdb9938ad965eadcc
Samuelgs155 <samuelgs15@hotmail.com>
2021-02-21 09:27:41 +0100

    Add files via upload

---
+++ JavaPublisher/src/main/java/com/demo/publisher/Publisher.java
@@ -0,0 +1,48 @@
+package com.demo.publisher;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+public class Publisher {
+
+	public static void main(String[] args) {
+		ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", 
+				"tcp://localhost:61616");
+		
+		try {
+			Connection connection = factory.createConnection();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = session.createQueue("demo");
+			
+			String message = "First Message";
+			MessageProducer producer = session.createProducer(destination);
+			TextMessage textMessage = session.createTextMessage(message);
+			producer.send(textMessage);
+			
+//			String[] messages = {"First Message", "Second Message", "Thrid Message", 
+//					"Fourth Message"};
+//			
+//			MessageProducer producer = session.createProducer(destination);
+//			
+//			for (String message : messages) {
+//				TextMessage textMessage = session.createTextMessage(message);
+//				producer.send(textMessage);
+//			}
+			
+			System.out.println("Message Published");
+			
+			session.close();
+			connection.close();
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ JavaPublisher/src/main/java/com/demo/publisher/RealTimeExample.java
@@ -0,0 +1,44 @@
+package com.demo.publisher;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.json.JSONObject;
+
+public class RealTimeExample {
+
+	public static void main(String[] args) {
+		ConnectionFactory factory = new ActiveMQConnectionFactory("admin", "admin", 
+				"tcp://localhost:61616");
+		
+		try {
+			Connection connection = factory.createConnection();
+			Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			Destination destination = session.createQueue("demo");
+			
+			JSONObject json = new JSONObject();
+			json.put("from_date", "01-Jan-2019");
+			json.put("to_date", "31-Dec-2019");
+			json.put("email", "xyz@gmail.com");
+			json.put("query", "select * from data");
+			
+			TextMessage textMessage = session.createTextMessage(json.toString());
+			
+			MessageProducer producer = session.createProducer(destination);
+			producer.send(textMessage);
+			
+			session.close();
+			connection.close();
+			
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
